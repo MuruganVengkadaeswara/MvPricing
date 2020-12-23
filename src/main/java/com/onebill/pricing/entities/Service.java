9b7c29a7b573313bb82e62cf32e925ffa660a625
onebill <onebill@onebill-VPCSE17GG>
2020-12-23 07:09:42 -0500

    Commit 1739

--- src/main/java/com/onebill/pricing/entities/Service.java
+++ src/main/java/com/onebill/pricing/entities/Service.java
@@ -1,20 +1,13 @@
 package com.onebill.pricing.entities;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 
 import lombok.Data;
 
